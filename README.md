9f0985dd043b9abc0a99682ec909d8dbabd5e476
HanJinLiang <hanjinliang1992@163.com>
2017-07-06 10:44:07 +0800

    自定义属性使用完善 编辑ReadMe

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # NetsScoreView
-NetsScoreView一个网状评分自定义控件
-# 使用说明
+NetsScoreView一个网状评分自定义控件，支持多种自定义属性
+# 先看看效果吧
+![img](https://github.com/HanJinLiang/NetsScoreView/blob/master/NetsScoreView.gif)
+# 集成步骤
 ## 在根项目gradle添加
 ``` java
 allprojects {
@@ -16,3 +18,50 @@ allprojects {
 	        compile 'com.github.HanJinLiang:NetsScoreView:V1.0.0'
 	}
 ```
+# 使用说明
+在XML布局直接使用
+```xml
+<com.hanjinliang.netsscore.NetsScoreView
+        android:id="@+id/NetsScoreView"
+        android:layout_width="300dp"
+        android:layout_height="300dp"
+        netsscore:NetsColor="#afafaf"
+        netsscore:ScoreFillColor="#60FF4081"
+        netsscore:ScoreLineColor="#FF4081"
+        netsscore:ScoreTxtColor="#2b2b2b"
+        netsscore:MaxScore="100"
+        netsscore:EdgeCount="6"
+        netsscore:TxtSize="14sp" />
+```
+也可以直接new NetsScoreView() 使用
+### 设置数据源
+``` java
+     /**
+     * 设置数据源
+     * @param edgeCount 多边形变数
+     * @param scores  数据源
+     */
+    public void setData(int edgeCount,float[] scores){}
+```
+### 支持自定义分数Format
+``` 
+    mNetsScoreView.setTxtFormat(new NetsScoreView.TxtFormat() {
+            @Override
+            public String originalDataFormat(int index, float value) {
+                return value+"分数";//格式化显示数值
+            }
+        });
+```
+## 自定义属性说明
+| 属性名称       | 解释         |
+| ------------- |:-------------:|
+| NetsColor     | 网格线的颜色 | 
+| ScoreLineColor| 分数覆盖物边缘线颜色|
+| ScoreFillColor | 分数覆盖物填充颜色     |
+| EdgeCount | 多边形边数     |
+| MaxScore | 分数最大值    |
+| ScoreTxtColor | 分数字体颜色|
+| TxtSize | 分数字体大小     |
+
+## 其他
+参考自[SpiderWebScoreView](https://github.com/xiaopansky/SpiderWebScoreView)，纯粹当做自己练习
--- netsscore/src/main/java/com/hanjinliang/netsscore/NetsScoreView.java
+++ netsscore/src/main/java/com/hanjinliang/netsscore/NetsScoreView.java
@@ -9,6 +9,7 @@ import android.graphics.Path;
 import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 
@@ -17,30 +18,30 @@ import android.view.View;
  * 网状评分控件
  */
 public class NetsScoreView extends View {
-    private int mWidth;
-    private int mHeight;
-    private float mRadius;
+    private int mWidth;//宽度
+    private int mHeight;//高度
+    private float mRadius;//半径  只是网格半径不包括文字部分
     private int mEdgeCount=5;//多边形边数
     private int mMaxScore=100;//最大值
     private float[] scores;//分值
     private Context mContext;
 
     private Path mPath;
-    private Paint mNetsPaint;
-    private int mNetsColor;
-    private Paint mScoreLinePaint;
-    private int mScoreLineColor;
-    private Paint mScoreFillPaint;
-    private int mScoreFillColor;
+    private Paint mNetsPaint;//网格Paint
+    private int mNetsColor;//网格颜色
+    private Paint mScoreLinePaint;//覆盖外延线Paint
+    private int mScoreLineColor;//覆盖外延线Color
+    private Paint mScoreFillPaint;//覆盖填充Paint
+    private int mScoreFillColor;//覆盖填充Color
 
-    private Paint mScoreTxtPaint;
-    private int mScoreTxtColor;
+    private Paint mScoreTxtPaint;//分数Color
+    private int mScoreTxtColor;//分数Color
 
-    private float mTxtMaxLength;
-    private float mTxtHeight;
+    private float mTxtMaxLength;//分数里面最大长度
+    private float mTxtHeight;//分数文字高度
 
-    public int mLineWidth;
-    public float mTxtSize;
+    public int mLineWidth;//线的宽度  默认1dp
+    public float mTxtSize;//分数Size
 
     public NetsScoreView(Context context) {
         this(context,null);
@@ -53,14 +54,18 @@ public class NetsScoreView extends View {
     public NetsScoreView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         mContext=context;
-
+        //获取自定义属性
         TypedArray ta=context.obtainStyledAttributes(attrs, R.styleable.NetsScoreView,defStyleAttr,0);
         mNetsColor=ta.getColor(R.styleable.NetsScoreView_NetsColor, Color.GRAY);
         mScoreLineColor=ta.getColor(R.styleable.NetsScoreView_ScoreLineColor, Color.RED);
         mScoreFillColor=ta.getColor(R.styleable.NetsScoreView_ScoreFillColor,Color.argb(100,255,0,0));
-        mScoreTxtColor=ta.getColor(R.styleable.NetsScoreView_ScoreFillColor,Color.GRAY);
+        mScoreTxtColor=ta.getColor(R.styleable.NetsScoreView_ScoreTxtColor,Color.GRAY);
         mTxtSize=ta.getDimension(R.styleable.NetsScoreView_TxtSize,sp2px(12));
         mEdgeCount=ta.getInt(R.styleable.NetsScoreView_EdgeCount,5);
+        if(mEdgeCount<3){
+            mEdgeCount=3;//最少需要3个边
+        }
+        mMaxScore=ta.getInt(R.styleable.NetsScoreView_MaxScore,100);
         ta.recycle();
 
         mNetsPaint=new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -92,67 +97,30 @@ public class NetsScoreView extends View {
         mTxtMaxLength=getTxtMaxLength();
     }
 
-    public void setData(int edgeCount,float[] scores){
-        mEdgeCount=edgeCount;
-        this.scores=scores;
-        if(mEdgeCount!=scores.length){
-            try {
-                throw new Exception("变数和分数数组长度必须一致");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return;
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int widthMode=MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize=MeasureSpec.getSize(widthMeasureSpec);
+        int heightMode=MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize=MeasureSpec.getSize(heightMeasureSpec);
+        int width,height,result;
+
+        //计算宽度
+        if(widthMode==MeasureSpec.EXACTLY){//MatchParent
+            width=widthSize;
+        }else{//默认高度
+            width=dp2px(300);//默认宽度300dp
         }
-        invalidate();
-    }
 
-    public void setMaxScore(int maxScore) {
-        mMaxScore = maxScore;
-        invalidate();
-    }
-
-    /**
-     * 分数文字最大的值
-     * @return
-     */
-    private float getTxtMaxLength(){
-        Rect rect=new Rect();
-        float maxLength=0;
-        for(int i=0;i<mEdgeCount;i++){
-            mScoreTxtPaint.getTextBounds(getShowTxt(i),0,getShowTxt(i).length(),rect);
-            if(rect.width()>maxLength){
-                maxLength=rect.width();
-            }
+        //计算高度
+        if(heightMode==MeasureSpec.EXACTLY){//MatchParent
+            height=heightSize;
+        }else{//默认高度
+            height=dp2px(300);//默认高度300dp
         }
-        mTxtHeight=rect.height();
-        return maxLength;
-    }
-
-    /**
-     * 根据分数获取要显示的txt
-     * @param index
-     * @return
-     */
-    private String getShowTxt(int index){
-        if(mTxtFormat==null){
-            return scores[index]+"";
-        }
-        return  mTxtFormat.originalDataFormat(index,scores[index]);
-    }
-
-    TxtFormat mTxtFormat;
-    public void setTxtFormat(TxtFormat txtFormat) {
-        //根据用户自定义format 重新计算最大宽度 以及半径
-        mTxtFormat = txtFormat;
-        mTxtMaxLength=getTxtMaxLength();
-        mRadius=mWidth/2-mTxtMaxLength;
-    }
-
-    /**
-     * 格式化显示数值
-     */
-    public interface TxtFormat{
-        public String originalDataFormat(int index, float value);
+        result=Math.min(width,height);
+        setMeasuredDimension(result,result);
     }
 
     /**
@@ -241,7 +209,6 @@ public class NetsScoreView extends View {
             nextRadians = (float) Math.toRadians(nextAngle);
             nextPointX = (float) ( Math.sin(nextRadians) * radius);
             nextPointY = (float) ( Math.cos(nextRadians) * radius);
-            canvas.drawPoint(nextPointX,nextPointY,mScoreFillPaint);
             if(position == 0){
                 mPath.moveTo(nextPointX, nextPointY);
             }else{
@@ -309,6 +276,90 @@ public class NetsScoreView extends View {
             //画圆心到个顶点的连线
             canvas.drawLine(0, 0, nextPointX, nextPointY, mNetsPaint);
         }
+    }
+
+
+
+
+    /**
+     * 分数文字最大的值
+     * @return
+     */
+    private float getTxtMaxLength(){
+        Rect rect=new Rect();
+        float maxLength=0;
+        for(int i=0;i<mEdgeCount;i++){
+            mScoreTxtPaint.getTextBounds(getShowTxt(i),0,getShowTxt(i).length(),rect);
+            if(rect.width()>maxLength){
+                maxLength=rect.width();
+            }
+        }
+        mTxtHeight=rect.height();
+        return maxLength;
+    }
+
+    /**
+     * 根据分数获取要显示的txt
+     * @param index
+     * @return
+     */
+    private String getShowTxt(int index){
+        if(mTxtFormat==null){
+            return scores[index]+"";
+        }
+        return  mTxtFormat.originalDataFormat(index,scores[index]);
+    }
+
+    TxtFormat mTxtFormat;
+
+    /**
+     * 设置用户自定义Format
+     * @param txtFormat
+     */
+    public void setTxtFormat(TxtFormat txtFormat) {
+        //根据用户自定义format 重新计算最大宽度 以及半径
+        mTxtFormat = txtFormat;
+        mTxtMaxLength=getTxtMaxLength();
+        mRadius=mWidth/2-mTxtMaxLength;
+    }
+
+    /**
+     * 格式化显示数值
+     */
+    public interface TxtFormat{
+        public String originalDataFormat(int index, float value);
+    }
+
+    /**
+     * 设置数据源
+     * @param edgeCount 多边形变数
+     * @param scores  数据源
+     */
+    public void setData(int edgeCount,float[] scores){
+        if(edgeCount<3){
+            Log.e("NetsScoreView","最少需要三条边");
+            return;
+        }
+        mEdgeCount=edgeCount;
+        this.scores=scores;
+        if(mEdgeCount!=scores.length){
+            Log.e("NetsScoreView","边数和分数数组长度必须一致");
+            return;
+        }
+        invalidate();
+    }
+
+    /**
+     * 设置数据源  默认xml里面变数
+     * @param scores  数据源
+     */
+    public void setData(float[] scores){
+        setData(mEdgeCount,scores);
+    }
+
+    public void setMaxScore(int maxScore) {
+        mMaxScore = maxScore;
+        invalidate();
     }
 
 }
